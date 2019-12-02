62b9e21bedb17bb0f0ce4f240c648e12830fa98a
robinbruer <r_bruer@hotmail.com>
2019-12-02 15:23:35 +0100

    Initial commit

---
+++ Labb3/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Labb3
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void CalculateButton(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(input.Text))
+            {
+                string buttonText = ((Button)sender).Text;
+                double vatRate = double.Parse(buttonText.Remove(buttonText.Length - 1));
+                double inputDouble = double.Parse(input.Text);
+
+                rateLabel.Text = buttonText;
+                enterdAmountLabel.Text = input.Text + " SEK";
+
+                double amount = inputDouble / (1 + (vatRate / 100));
+                double vat = inputDouble - amount;
+
+                calculatedAmountLabel.Text = amount.ToString("0.00") + " SEK";
+                outputAmountntLabel.Text = vat.ToString("0.00") + " SEK";
+            }
+        }
+
+    }
+}
